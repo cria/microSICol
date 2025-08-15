a005b6b30d7eb022e0b0d6023f64deb780bc7fd8
regiov <renato@cria.org.br>
2025-08-15 14:31:58 -0300

    Mais documentação sobre uso da lib e ajustes em get_dict

--- py/modules/dom_xml.py
+++ py/modules/dom_xml.py
@@ -11,20 +11,34 @@ from bs4 import BeautifulSoup
 
 
 class Xml(object):
-    '''Aparentemente essa lib foi criada para lidar com arquivos XML com uma tag
-       root e subtags num único nível sempre com o mesmo nome, como é o caso do
-       arquivo de configuração do sistema (config.xml):
+    '''Aparentemente essa lib foi criada para lidar com dois padrões de arquivos 
+       XML, ambos com apenas um nível abaixo da tag principal e sempre com o mesmo
+       nome de tag. O primeiro padrão é utilizado pelo arquivo de configuração do 
+       sistema (config.xml):
        <configs>
          <config name="config1">some value1</config>
          <config name="config2">some value2</config>
        </configs>
        O método "get" busca o conteúdo da tag com base no valor do atributo "name":
-       xml = Xml('subtag', '/path/to/xml/file')
+       xml = Xml('config', xml_content)
        config1 = xml.get('config1') # <- some value1
+       O outro padrão é usado na configuração dos relatórios:
+       <fields>
+         <field name="id_doc" label="label_Rep_Doc_ID" data_type="integer" aggregate_function="count" />
+         <field name="code" label="label_Rep_Doc_Code" data_type="varchar" aggregate_function="count" />
+         <field name="qualifier" label="label_Rep_Doc_Qualifier" data_type="varchar" aggregate_function="count" />
+      </fields>
+      Para este padrão costuma-se usar o método get_dict:
+      xml = Xml('config', xml_content)
+      fields_def = xml.get_dict('name', ['label', 'data_type'])
+      # {'id_doc': {'label': 'label_Rep_Doc_ID', 'data_type': 'integer'},
+         'code': {'label': 'label_Rep_Doc_Code', 'data_type': 'varchar'},
+         ...
+      # }
     '''
-    def __init__(self, tag_name, file):
+    def __init__(self, tag_name, content):
         # Use 'xml' parser for XML files
-        self.soup = BeautifulSoup(file, 'xml')
+        self.soup = BeautifulSoup(content, 'xml')
         self.tag_name = tag_name
 
     def get(self, name):
@@ -37,14 +51,10 @@ class Xml(object):
     def get_dict(self, label, internal_labels):
         # Retorna um dicionário com o valor de 'label' como chave e os internos como dict
         result = {}
-        if self.root is None:
-            return result
-        for item in self.root.find_all(label):
+        for item in self.soup.find_all(self.tag_name):
             tmp = {}
             for int_label in internal_labels:
-                val = item.find(int_label)
-                if val is not None:
-                    tmp[int_label] = val.get_text(strip=True)
-            key = item.get_text(strip=True) if not item.has_attr(label) else item[label]
-            result[key] = tmp
+                if item.has_attr(int_label):
+                    tmp[int_label] = item[int_label]
+            result[item[label]] = tmp
         return result
